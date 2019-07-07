a548cb2034f8ce97b58827459427696fae204596
nikhilgupta12 <nikhilgupta1999@gmail.com>
2019-07-07 18:51:22 +0530

    Uploading

---
+++ precache-manifest.df76227d9df9940114fe397d949a1183.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5e4366279872348adaf6c702a262de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fe4fd708e9ae95a48e81",
+    "url": "/static/css/main.cb55ba7c.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "fe4fd708e9ae95a48e81",
+    "url": "/static/js/main.68ceac0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
